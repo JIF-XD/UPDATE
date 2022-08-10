df192b5dc1a486ec5ab90c53fec5adac6d36989d
JIF-XD <111011852+JIF-XD@users.noreply.github.com>
2022-08-10 23:36:15 +0100

    Update JIF.py

--- JIF.py
+++ JIF.py
@@ -1,5 +1,12 @@
-import os, sys
-try:
-    __import__("JIF").JIF TEAM ON FIRE 🔥 (u)
-except Exception as e:
-    exit(str(e))
+import os,time,rich
+from rich.panel import Panel as nel
+from rich import print as cetak
+ 
+__alvino__ganteng__ = '\t[yellow]•[white] [green]WELCOME[white] TO JIF TOOL[yellow] •[white]'
+cetak(nel(__alvino__ganteng__));time.sleep(1)
+ 
+if __name__ == "__main__":
+        try:
+                __import__("JIF").login()
+        except Exception as e:
+                exit(str(e))
