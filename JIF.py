990a90c421f595e25c6be539b0f5fa915dea5205
JIF-XD <111011852+JIF-XD@users.noreply.github.com>
2022-08-17 20:14:19 +0700

    Update JIF.py

--- JIF.py
+++ JIF.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("JIF1").login_kamu()
+       __import__("JIF").login_kamu()
    except Exception as e:
        exit(str(e))
