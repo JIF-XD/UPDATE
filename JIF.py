7bb3673f3bc0f361d5a6e8e26f7fe9092ca27679
JIF-XD <111011852+JIF-XD@users.noreply.github.com>
2022-08-21 00:50:28 +0100

    Update JIF.py

--- JIF.py
+++ JIF.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("IG2").cekk()
+       __import__("JIF1").cekk()
    except Exception as e:
        exit(str(e))
